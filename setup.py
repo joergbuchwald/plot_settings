5653f193ac34f47f9c5b0646625f788ea068e5e4
joergbuchwald <joerg.buchwald@ufz.de>
2022-01-21 11:28:03 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(name="plot_settings",
+      version=0.1,
+      maintainer="Jörg Buchwald",
+      maintainer_email="joerg_buchwald@ufz.de",
+      author="Jörg Buchwald",
+      author_email="joerg.buchwald@ufz.de",
+      install_requires=["matplotlib"],
+      py_modules=["plot_settings"],
+      packages=[])
